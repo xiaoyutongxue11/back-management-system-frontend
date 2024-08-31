abe13017d51c7ebecde48270ddff649bcdd15386
tyy123 <1572235056@qq.com>
2024-08-31 17:25:51 +0800

    element-plus国际化

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 
 const app = createApp(App);
-app.use(router).mount("#app");
+app
+  .use(router)
+  .use(ElementPlus, {
+    locale: zhCn,
+  })
+  .mount("#app");
