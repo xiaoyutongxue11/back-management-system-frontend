3d7e4bd26b49e6ede144397cbb55e6decadcf0e9
tyy123 <1572235056@qq.com>
2024-09-01 18:58:07 +0800

    注册路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [{}];
+const routes = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    name: "login",
+    path: "/login",
+    component: () => import("@/views/login/index.vue"),
+  },
+];
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
