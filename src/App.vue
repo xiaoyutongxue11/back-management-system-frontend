fd16a51538ee0ba8f4d09d705c0bc5686d269513
tyy123 <1572235056@qq.com>
2024-09-01 18:56:59 +0800

    清除一些默认样式

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,6 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import { ElButton } from "element-plus";
-</script>
-
+<script setup lang="ts"></script>
 <template>
-  <div>
-    <ElButton>按钮</ElButton>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <div class="div1">
-    1111
-    <div class="div2">2222</div>
-  </div>
+  <RouterView></RouterView>
 </template>
 
-<style scoped lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-.div1 {
-  background-color: red;
-  .div2 {
-    background-color: blue;
-  }
-}
-</style>
+<style lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import "./style.css";
 import router from "./router";
 import ElementPlus from "element-plus";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
